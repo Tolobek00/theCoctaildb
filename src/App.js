16d2fac9c6af2d9b32b9a8a827f7f75911cd496e
Tolobek <tsharsheev002@gmail.com>
2024-07-16 15:54:26 +0600

    add adaptive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import Layout from "./Components/Layout/Layout";
 import HomePages from "./Pages/HomePages";
 import Popular from "./Components/PopularDrinks/Popular";
 import Category from "./Components/Category/Category";
+import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
